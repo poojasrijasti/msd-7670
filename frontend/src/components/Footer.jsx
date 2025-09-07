ba1412c65d46c87c15c1b0e454bb0b5a8f81cbbd
poojasrijasti <231FA04767@gmail.com>
2025-09-07 21:23:10 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>MediChain is a secure, transparent, and decentralized healthcare management system built on blockchain technology. It aims to overcome challenges in the current healthcare sector such as fragmented medical records, lack of data security, slow information sharing, and unauthorized access. By leveraging blockchain, MediChain ensures data integrity, privacy, and accessibility for patients, doctors, and healthcare institutions.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 6309497466</li>
+            <li>Medichain+123@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ Medichain+123@gmail.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
